d29e6a32db28a605ca2bf5dad74189fa2926cb96
Delta Student <Deltastudent@gmail.com>
2025-12-22 11:09:32 +0530

    Updated Product.jsx pages

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,7 +4,7 @@ import ProductCard from '../Components/ProductCard'
 function Product() {
   return (
     <>
-    <div>
+    <div className="pt-24 px-6 max-w-7xl mx-auto pb-24">
     <ProductCard/>
     </div>
     </>
